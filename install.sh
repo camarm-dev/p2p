97a91441d35e4911d286f8615ed083197d4af51c
camarm <armand.camponovo@icloud.com>
2023-04-14 08:42:25 +0000

    slient curls

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ "$(whoami)" = "root" ]; then
   echo "Installing p2p in /usr/bin"
-  curl https://github.com/releases/lastest/ -o /usr/bin/p2p
+  curl https://github.com/releases/lastest/ -s -o /usr/bin/p2p
   chmod +x /usr/bin/p2p
   echo "Creating data folder ~/.p2p"
   mkdir -p ~/.p2p
   echo "Writting default configuration"
-  curl https://raw.githubusercontent.com/camarm-dev/p2p/main/config.default.json > ~/.p2p/config.json
+  curl https://raw.githubusercontent.com/camarm-dev/p2p/main/config.default.json -s -o ~/.p2p/config.json
   echo "Running p2p info"
   p2p info
   echo "The p2p executable is installed !"
