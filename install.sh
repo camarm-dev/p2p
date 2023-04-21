d8b8caed3eb858470bd73e64474ae1bebc72537d
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-04-21 18:53:12 +0200

    Misspelled a word

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ "$(whoami)" = "root" ]; then
   chmod +x /usr/bin/p2p
   echo "Creating data folder ~/.p2p"
   mkdir -p ~/.p2p
-  echo "Writting default configuration"
+  echo "Writing default configuration"
   curl https://raw.githubusercontent.com/camarm-dev/p2p/main/config.default.json -s -o ~/.p2p/config.json
   echo "Running p2p info"
   p2p info
