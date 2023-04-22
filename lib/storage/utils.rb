a2fbf5cacf951e1683b0aff78660f17701b961e8
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-04-22 11:31:30 +0200

    Safe storage utils

--- lib/storage/utils.rb
+++ lib/storage/utils.rb
@@ -3,11 +3,26 @@ require 'json'
 module Storage
     FOLDER = "#{Dir.home}/.p2p"
 
+    def self.default_file(name)
+        path = "#{FOLDER}/#{name}.json"
+        unless File.exist?(path)
+            File.new(path, "w")
+            data = {
+              "data" => []
+            }
+            File.write("#{FOLDER}/#{name}.json", JSON.dump(data))
+        end
+    end
+
     def self.write(data, name)
+        (name)
         File.write("#{FOLDER}/#{name}.json", JSON.dump(data))
     end
 
     def self.read(name)
+        default_file(name)
         return JSON.parse(File.read("#{FOLDER}/#{name}.json"))
     end
+
+    private_class_method :default_file
 end
