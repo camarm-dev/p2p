724a126a2f0f44eb1e883b1e15bf0a12f181d85c
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-04-22 12:56:08 +0200

    Using right user for home folder

--- scripts/install-packed.sh
+++ scripts/install-packed.sh
@@ -6,11 +6,11 @@ if [ "$(whoami)" = "root" ]; then
   echo "Installing p2p libraries in /usr/lib"
   cp -r packed/p2p-libs /usr/lib/p2p
   echo "Creating data folder ~/.p2p"
-  mkdir -p ~/.p2p
+  su $USER -c 'mkdir -p ~/.p2p'
   echo "Writing default configuration"
-  curl https://raw.githubusercontent.com/camarm-dev/p2p/main/config.default.json -s -o ~/.p2p/config.json
+  su $USER -c 'curl https://raw.githubusercontent.com/camarm-dev/p2p/main/config.default.json -s -o ~/.p2p/config.json'
   echo "Running p2p info"
-  p2p info
+  su $USER -c 'p2p info'
   echo "The p2p executable is installed !"
 else
   echo "Run this as root"
