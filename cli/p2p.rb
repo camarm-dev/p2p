a8e03880f7194587124391c3a064cfd901f715be
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-04-22 11:34:47 +0200

    thor long desc and failure handling

--- cli/p2p.rb
+++ cli/p2p.rb
@@ -9,9 +9,19 @@ class P2P < Thor
   desc "P2P Cli", "The easiest way to push to production any application in one command !"
 
   desc "info", "Test your current p2p installation."
+  long_desc <<-LONGDESC
+    `p2p info` will print out informations about your current p2p installation.
+
+    > $ p2p info
+  LONGDESC
   def info
     puts "Installed path: #{__FILE__}"
     puts "Version: #{CONFIG["version"]}"
+  end
+
+  def Thor.exit_on_failure?
+    puts "Fatal error ❌"
+    exit 1
   end
 
   desc "servers", "P2p servers utilities"
